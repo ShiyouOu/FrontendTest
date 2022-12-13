ec2646433ce7a692660738493d2839aa29a9b8f5
ShiyouOu <54868441+ShiyouOu@users.noreply.github.com>
2022-12-13 09:36:23 -0500

    1 file modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,16 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './core/components/navbar/navbar.component';
 import { LoginComponent } from './core/components/account/login.component';
+import { EmployeesComponent } from './core/components/hr/employees.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, NavbarComponent, LoginComponent],
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    EmployeesComponent,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
