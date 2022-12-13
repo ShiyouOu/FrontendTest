621fa20fdfea4c8604915a87841d3a962dffa33f
ShiyouOu <54868441+ShiyouOu@users.noreply.github.com>
2022-12-13 10:48:20 -0500

    3 files modified, 1 added

--- src/app/core/components/hr/generatetoken.component.ts
+++ src/app/core/components/hr/generatetoken.component.ts
@@ -1,7 +1,22 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'my-token-generator',
   templateUrl: './generatetoken.component.html',
 })
-export class GenerateTokenComponent {}
+export class GenerateTokenComponent {
+  constructor(private http: HttpClient) {}
+
+  generateNewToken() {
+    const email: string = (<HTMLInputElement>(
+      document.getElementById('inputEmail')
+    )).value;
+
+    this.http
+      .get('http://localhost:8090/authentication-service/newtoken/' + email)
+      .subscribe((response) => {
+        console.log(response);
+      });
+  }
+}
